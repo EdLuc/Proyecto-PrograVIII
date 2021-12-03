5266705adcda4f70bc0b677f13f1e656aaec3f85
Lucas <edgarlucas20@gmail.com>
2021-12-03 17:16:27 -0600

    Actualización

---
+++ lib/models/models.dart
@@ -0,0 +1,6 @@
+export 'package:cartelera/models/movie.dart';
+export 'package:cartelera/models/top_rated_response.dart';
+export 'package:cartelera/models/now_playing_response.dart';
+export 'package:cartelera/models/similar_movies_response.dart';
+export 'package:cartelera/models/cast.dart';
+export 'package:cartelera/models/credits_response.dart';
